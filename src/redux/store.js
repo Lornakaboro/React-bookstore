82436911f9390d5de04c7ea80074f4c260d0e39e
Lornakaboro <lornakaboro@gmail.com>
2023-04-05 12:19:33 +0300

    Configure a redux store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import bookSlice from './books/booksSlice';
+import categoriesSlice from './categories/categoriesSlice';
+
+const store = configureStore({
+  reducer: {
+    book: bookSlice,
+    categories: categoriesSlice,
+  },
+});
+
+export default store;
